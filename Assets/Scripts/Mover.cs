6724a89cb5006f516751f0249d74523296640dac
Cemre <110540754+Rainy-Mask@users.noreply.github.com>
2023-09-28 20:35:15 +0300

    Ray Casting

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,9 +7,16 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField]  Transform target;
+    Ray lastRay;
 
      public void Update()
      {
+         if (Input.GetMouseButtonDown(0))
+         {
+             lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+         }
+         Debug.DrawRay(lastRay.origin, lastRay.direction * 100, Color.black);
+         
          GetComponent<NavMeshAgent>().destination = target.position;
      }
 }
